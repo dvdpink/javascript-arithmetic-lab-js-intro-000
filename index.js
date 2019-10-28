3419d81133c1f5c8d2832a861b8c3246d3e46c65
David Pink <dvdpink@gmail.com>
2019-10-28 16:37:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ function add (a,b) {return a + b}
 function subtract(a,b) {return a - b}
 function multiply(a,b) {return a * b}
 function divide(a,b) {return a / b}
+var n = 918
 function inc(n) {return 918++}
